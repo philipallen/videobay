3fff573defcac6b62179d571d546ece1a82c92ae
Phil Allen <phil.allen@imsmaxims.com>
2017-06-01 14:57:28 +0100

    Added loader to login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,48 +1,60 @@
 import { Component, OnInit } from '@angular/core';
- 
+
 import { AlertService } from '../../services/alert.service';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Nav, NavController } from 'ionic-angular';
+import { Nav, NavController, LoadingController, AlertController } from 'ionic-angular';
 
 import { HomePage } from '../home/home';
 import { RegisterPage } from '../register/register';
- 
+
 @Component({
-  	selector: 'page-login',
-    templateUrl: 'login.html'
+	selector: 'page-login',
+	templateUrl: 'login.html'
 })
- 
+
 export class LoginPage implements OnInit {
-    model: any = {};
-    loading = false;
-    returnUrl: string;
- 
-    constructor(
+	model: any = {};
+	loading = false;
+	returnUrl: string;
+
+	constructor(
+		private loadingCtrl: LoadingController,
+		private alertCtrl: AlertController,
 		public navCtrl: NavController,
 		public nav: Nav,
-        private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
- 
-    ngOnInit() {
-        this.authenticationService.logout();
-    }
- 
-    login() {
-        this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-					// this.navCtrl.push(HomePage, {});
+		private authenticationService: AuthenticationService,
+		private alertService: AlertService) { }
+
+	ngOnInit() {
+		this.authenticationService.logout();
+	}
+
+	login() {
+		let loadingPopup = this.loadingCtrl.create({
+	      	// content: 'Loading data...' //Can add text content here
+	      	// dismissOnPageChange: true
+	    });
+	    
+		loadingPopup.present();
+		this.authenticationService.login(this.model.username, this.model.password)
+			.subscribe(
+				data => {
     				this.nav.setRoot(HomePage);
-                },
-                error => {
-                    this.alertService.error(error);
-                    this.loading = false;
-                });
-    }
+				},
+				error => {
+					let alert = this.alertCtrl.create({
+					    title: 'Login failed',
+					    message: error,
+				    	buttons: ['OK']
+				  	});
+					loadingPopup.dismiss();
+					//this.alertService.error(error);
+					alert.present();
+				});
+	}
 
 	toRegisterPage() {
-	    this.navCtrl.push(RegisterPage);
+		this.navCtrl.push(RegisterPage);
 	}
 
 	// ionViewCanLeave(): boolean{
