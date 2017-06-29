b0b20f8a01af385694d1b63784a1f654f66d7684
Arturoo <synowiec.artur85@gmail.com>
2017-06-29 20:04:39 +0100

    Registration logic implemented

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
 
 import { AlertService } from '../../services/alert.service';
 import { UserService } from '../../services/user.service';
@@ -15,26 +15,38 @@ export class RegisterPage {
 
   	model: any = {};
     loading = false;
- 
+
     constructor(
     	public navCtrl: NavController,
         // private router: Router,
+        private alertCtrl: AlertController,
         private userService: UserService,
         private alertService: AlertService) { }
- 
+
     register() {
         this.loading = true;
+        console.log(this.model);
         this.userService.create(this.model)
             .subscribe(
                 data => {
                     // set success message and pass true paramater to persist the message after redirecting to the login page
                     this.alertService.success('Registration successful', true);
+                    console.log(data);
                     // this.router.navigate(['/login']);
                     this.navCtrl.push(LoginPage, {});
                 },
                 error => {
-                    this.alertService.error(error);
-                    this.loading = false;
+                    console.log(error);
+                    let alert = this.alertCtrl.create({
+                        title: 'Registration failed',
+                        message: error._body,
+                        buttons: ['OK']
+                      });
+
+                    //this.alertService.error(error);
+                    alert.present();
+                    //this.alertService.error(error);
+                    //this.loading = false;
                 });
     }
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
- 
+
 import { User } from '../models/users';
- 
+
+
+
 @Injectable()
 export class UserService {
+
+     //private baseUrl = 'http://81.169.165.110:1099/videobay/api/';
+
+    private baseUrl = 'http://localhost:8080/videobay/api/';
     constructor(private http: Http) { }
-     
+
     someoneLoggedIn() {
         return JSON.parse(localStorage.getItem('currentUser')) ? true : false;
     }
@@ -18,25 +24,33 @@ export class UserService {
     getAll() {
         return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
     }
- 
+
     getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.get('/api/user/' + id, this.jwt()).map((response: Response) => response.json());
     }
- 
+
     create(user: User) {
-        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post(this.baseUrl + 'user', user).map((response: Response) => response.json());
     }
- 
+
+
     update(user: User) {
         return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
     }
- 
+
     delete(id: number) {
         return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
     }
- 
+
+    getByUsernameAndPassword(user: User){
+      let params: URLSearchParams = new URLSearchParams();
+      params.set('user', user.screenName);
+      params.set('cnt', user.password);
+      return this.http.get(this.baseUrl + 'user' ,{search: params}).map((response: Response) => response.json())
+    }
+
     // private helper methods
- 
+
     private jwt() {
         // create authorization header with jwt token
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
