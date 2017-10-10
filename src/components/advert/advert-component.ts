2c13f1c0483b870b96cfd5a8be609bb5b37416ad
phil <philallen84@gmail.com>
2017-10-10 15:52:30 +0100

    Fixed video rendering

--- src/components/advert/advert-component.ts
+++ src/components/advert/advert-component.ts
@@ -1,18 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { AdvertsService } from '../../services/adverts.service';
-import videojs from 'video.js'
+import videojs from 'video.js';
 
 @Component({
     selector: 'advert-component',
     templateUrl: 'advert-component.html'
 })
 export class AdvertComponent {
+    @ViewChild('video') input: ElementRef; 
     response: any;
 	errorMessage: any;
     @Input() advert: any;
     @Input() user: any;
 
     constructor(private advertsService: AdvertsService) {}
+      
+    ngAfterViewInit() {
+        videojs(this.input.nativeElement);
+    }
 
 	toggleFavourite(advert) {
 		this.advertsService.addToFavourites(advert.id, this.user.id).subscribe(
@@ -26,7 +31,6 @@ export class AdvertComponent {
 	}
 
     //ANY VIDEO CONFIG OR LOGIC IN HERE
-  
     //TODO figure the below out...
     togglePause(e) {
         let myPlayer = videojs(e.srcElement);
