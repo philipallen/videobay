2c9a5de4b540bc1d1fb9628e81b322a92aacddc1
Arturoo <synowiec.artur85@gmail.com>
2018-01-08 19:03:34 +0000

    Strong advert type for Advert component

--- src/components/advert/advert-component.ts
+++ src/components/advert/advert-component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { Advert } from '../../models/advert';
 
 @Component({
     selector: 'advert-component',
     templateUrl: 'advert-component.html'
 })
 export class AdvertComponent {
-    @Input() advert: any;
+    @Input() advert: Advert;
 
     constructor() {}
 }
